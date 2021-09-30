93453a889c113c6eb42a8621da3063c6a93e7576
pgmann <pgmann@users.noreply.github.com>
2021-09-30 19:57:32 +0100

    Keep printing output until process dies

--- src/com/pgmann/commandprompt/RunCommand.java
+++ src/com/pgmann/commandprompt/RunCommand.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class RunCommand extends Thread {
 	String cmd;
 	Main ref;
+	PrintWriter writer;
 	
 	public RunCommand(String cmd, Main ref) {
 		this.cmd=cmd;
@@ -16,30 +17,29 @@ public class RunCommand extends Thread {
 		String s = null;
 		
 		try {
-
 			// log cmd
-
 			ref.log.setText(ref.log.getText()+cmd+"\n");
 			ref.updateDisplay();
 			
 			// run the command
-			
 			Process p = Runtime.getRuntime().exec(cmd);
 
 			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
 			BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+			this.writer = new PrintWriter(p.getOutputStream());
 
-			// read the output from the command
-			while ((s = stdInput.readLine()) != null) {
-				ref.log.setText(ref.log.getText()+s+"\n");
-				ref.updateDisplay();
-			}
+			while (p.isAlive()) {
+				// read the output from the command
+				while ((s = stdInput.readLine()) != null) {
+					ref.log.setText(ref.log.getText()+s+"\n");
+					ref.updateDisplay();
+				}
 
-			// read any errors from the attempted command
-			while ((s = stdError.readLine()) != null) {
-				ref.log.setText(ref.log.getText()+s+"\n");
-				ref.updateDisplay();
+				// read any errors from the attempted command
+				while ((s = stdError.readLine()) != null) {
+					ref.log.setText(ref.log.getText()+s+"\n");
+					ref.updateDisplay();
+				}
 			}
 		}
 		catch (IOException e) {
